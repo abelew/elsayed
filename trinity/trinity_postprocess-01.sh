8f0188453287e00d90895e79ed1a97a554bc6b2b
ashton trey belew <abelew@ibissub00.umiacs.umd.edu>
2013-02-19 10:00:13 -0500

    Added trinity submission scripts

---
+++ trinity/trinity_postprocess-01.sh
@@ -0,0 +1,69 @@
+#!/bin/env bash
+
+. $LAB/scripts/dotbashrc
+MYHIST=$LAB/scripts/.readline_trinity_args
+history -r $MYHIST
+history -a $MYHIST
+history -n $MYHIST
+
+echo "This script will attempt to submit a trinity denovo assembly."
+echo "Ideally, it is possible to change everything with readline,"
+echo "thus you should be able to hit up arrow to look at old arguments..."
+## Set initial arguments for pbs, trinity
+export PBS=" -V -S /cbcb/lab/nelsayed/local/bin/bash -q long -l walltime=144:00:00 -N trin_post -jeo -e ${WD}/trinity_postprocess.stdout "
+#export PBS=" -V -S /cbcb/lab/nelsayed/local/bin/bash -q high_throughput -N trinity -jeo -e ${WD}/trinity.stdout "
+export TRINITY="  --seqType fq --target ./trinity_out_dir/Trinity.fasta --aligner bowtie --retain_intermediate_files "
+export MYWD=$WD
+export R1=" --left ${MYWD}/"
+export R2=" --right ${MYWD}/"
+export LOG="./trinity_post.out"
+
+echo "$PBS"
+echo "If necessary, change the PBS_ARGS now:"
+read -e -i "${PBS}"
+export MY_PBS_ARGS=${REPLY%%*( )}
+history -s "$PBS_ARGS"
+
+echo "If necessary, change the Trinity arguments now:"
+read -e -i "${TRINITY}"
+export TRINITY_ARGS=${REPLY%%*( )}
+history -s "$TRINITY_ARGS"
+
+echo "Where are the read1 reads?:"
+read -e -i "${R1}"
+export R1=${REPLY%%*( )}
+history -s "$R1"
+
+echo "Where are the read2 reads?:"
+read -e -i "${R2}"
+export R2=${REPLY%%*( )}
+history -s "$R2"
+
+echo "Redirect STDOUT and STDERR to:"
+read -e -i "${LOG}"
+export LOG=${REPLY%%*( )}
+history -s "$LOG"
+
+history -w $MYHIST
+TRINITY="$PREFIX/trinity/util/alignReads.pl "
+export MYCMD="$TRINITY $TRINITY_ARGS $R1 $R2 2>$LOG 1>&2 "
+
+echo "Last chance to stop the madness:"
+echo "PBS_ARGS: $MY_PBS_ARGS"
+echo "CMD: $MYCMD"
+
+cat <<"EOF" | qsub $MY_PBS_ARGS -
+#!/bin/env bash
+
+. ${LAB}/scripts/dotbashrc
+
+cd $MYWD
+start_log
+echo "About to run:"
+echo "$MYCMD"
+sleep 5
+eval $MYCMD
+end_log
+EOF
+
+echo "JOB completed in $SECONDS"
